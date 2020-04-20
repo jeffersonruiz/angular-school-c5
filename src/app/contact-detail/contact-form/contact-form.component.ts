6afa8ca942109f323ead92db2104e783ea460e4a
farid <jefferson.farid@hotmai.com>
2020-04-20 18:57:38 -0500

    clase 88 state control css  20/04/2020

--- src/app/contact-detail/contact-form/contact-form.component.ts
+++ src/app/contact-detail/contact-form/contact-form.component.ts
@@ -10,30 +10,30 @@ import { ContactsService } from 'src/app/contacts.service';
 })
 export class ContactFormComponent implements OnInit {
 
-  public model:Contact = new Contact(0, '', 'assets/default-user.png', []);
-  public readonly phoneTypes:string[] = Object.values(PhoneType);
-  
-  constructor(private contactsService:ContactsService) { }
+  public model: Contact = new Contact(0, '', 'assets/default-user.png', []);
+  public readonly phoneTypes: string[] = Object.values(PhoneType);
+
+  constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
   }
 
-  addContact(){
+  addContact() {
     this.contactsService.addContact(this.model);
     this.model = new Contact(0, '', 'assets/default-user.png', []);
   }
 
-  addNewPhoneToModel(){
-    this.model.phones.push({type:null, number:null});
+  addNewPhoneToModel() {
+    this.model.phones.push({type: null, number: null});
   }
 
-  addImage(event){
+  addImage(event) {
     const file = event.target.files[0];
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (evt) => {
-      this.model.picture = <string>reader.result;
-    }
+      this.model.picture =  reader.result as string;
+    };
   }
 
 }
