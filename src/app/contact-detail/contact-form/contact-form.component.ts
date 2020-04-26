02bde3ac42b863ea8f76e4c8068c358d17db21a7
farid <jefferson.farid@hotmai.com>
2020-04-26 12:44:08 -0500

    clase 90 ngform  26/04/2020

--- src/app/contact-detail/contact-form/contact-form.component.ts
+++ src/app/contact-detail/contact-form/contact-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Contact, PhoneType } from 'src/app/contact.model';
 import { ContactsService } from 'src/app/contacts.service';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -10,30 +11,32 @@ import { ContactsService } from 'src/app/contacts.service';
 })
 export class ContactFormComponent implements OnInit {
 
-  public model:Contact = new Contact(0, '', 'assets/default-user.png', []);
-  public readonly phoneTypes:string[] = Object.values(PhoneType);
-  
-  constructor(private contactsService:ContactsService) { }
+  public model: Contact = new Contact(0, '', 'assets/default-user.png', []);
+  public readonly phoneTypes: string[] = Object.values(PhoneType);
+  @ViewChild('contactForm', {static: true}) contactForm: NgForm;
+
+  constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
   }
 
-  addContact(){
+  addContact() {
     this.contactsService.addContact(this.model);
     this.model = new Contact(0, '', 'assets/default-user.png', []);
+    this.contactForm.reset();
   }
 
-  addNewPhoneToModel(){
-    this.model.phones.push({type:null, number:null});
+  addNewPhoneToModel() {
+    this.model.phones.push({type: null, number: null});
   }
 
-  addImage(event){
+  addImage(event) {
     const file = event.target.files[0];
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (evt) => {
-      this.model.picture = <string>reader.result;
-    }
+      this.model.picture =  reader.result as string;
+    };
   }
 
 }
