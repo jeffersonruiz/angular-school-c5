3c4168c8ed68613ea336be7b4d1c2a65746411ec
farid <jefferson.farid@hotmai.com>
2020-04-28 18:49:08 -0500

    clase 9 validation   28/04/2020

--- src/app/contact-detail/contact-form/contact-form.component.ts
+++ src/app/contact-detail/contact-form/contact-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact, PhoneType } from 'src/app/contact.model';
 import { ContactsService } from 'src/app/contacts.service';
-import { FormControl, FormGroup } from '@angular/forms';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { startsWithCapitalValidator } from 'src/app/directives/startsWithCapital.directive';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,9 +10,9 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  public readonly phoneTypes:string[] = Object.values(PhoneType);
-  public contactForm:FormGroup = new FormGroup({
-    name: new FormControl(''),
+  public readonly phoneTypes: string[] = Object.values(PhoneType);
+  public contactForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2), startsWithCapitalValidator()]),
     picture: new FormControl('assets/default-user.png'),
     phone: new FormGroup({
       type: new FormControl(null),
@@ -22,26 +22,30 @@ export class ContactFormComponent implements OnInit {
     address: new FormControl('')
   });
 
-  constructor(private contactsService:ContactsService) { }
+  constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
   }
 
-  addContact(){
+  addContact() {
   }
 
-  addNewPhoneToModel(){
+  addNewPhoneToModel() {
   }
 
-  addImage(event){
+  addImage(event) {
     const file = event.target.files[0];
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (evt) => {
       this.contactForm.patchValue({
-        picture:reader.result
+        picture: reader.result
       });
-    }
+    };
+  }
+
+  get name(){
+    return this.contactForm.get('name');
   }
 
 }
