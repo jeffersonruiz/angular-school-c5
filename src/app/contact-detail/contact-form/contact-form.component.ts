36738fe879ec819c223d72b3a0fba1b65fb76931
farid <jefferson.farid@hotmai.com>
2020-04-27 19:07:03 -0500

    clase 94 form groups  26/04/2020

--- src/app/contact-detail/contact-form/contact-form.component.ts
+++ src/app/contact-detail/contact-form/contact-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact, PhoneType } from 'src/app/contact.model';
 import { ContactsService } from 'src/app/contacts.service';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -10,28 +10,31 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  public readonly phoneTypes:string[] = Object.values(PhoneType);
-  public name:FormControl = new FormControl('');
+  public readonly phoneTypes: string[] = Object.values(PhoneType);
+  public contactForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormControl('')
+  });
 
-  constructor(private contactsService:ContactsService) { }
+  constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
-    this.name.setValue('Peter');
   }
 
-  addContact(){
+  addContact() {
   }
 
-  addNewPhoneToModel(){
+  addNewPhoneToModel() {
   }
 
-  addImage(event){
+  addImage(event) {
     const file = event.target.files[0];
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (evt) => {
-      //save reader.result in form picture field
-    }
+      // save reader.result in form picture field
+    };
   }
 
 }
