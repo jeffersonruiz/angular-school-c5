958b06efe84e2730bf3e703fabc4f7337b55abdb
farid <jefferson.farid@hotmai.com>
2020-04-29 18:42:19 -0500

    clase 100 formarray  29/04/2020

--- src/app/contact-detail/contact-form/contact-form.component.ts
+++ src/app/contact-detail/contact-form/contact-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact, PhoneType } from 'src/app/contact.model';
 import { ContactsService } from 'src/app/contacts.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
 import { startsWithCapitalValidator } from 'src/app/directives/startsWithCapital.directive';
 import { tap, filter, map } from 'rxjs/operators';
 import { zip } from 'rxjs';
@@ -13,26 +13,33 @@ import { zip } from 'rxjs';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-  public readonly phoneTypes:string[] = Object.values(PhoneType);
-  public contactForm:FormGroup = new FormGroup({
+  public readonly phoneTypes: string[] = Object.values(PhoneType);
+  public contactForm: FormGroup = new FormGroup({
     name: new FormControl('', [ Validators.required, Validators.minLength(2), startsWithCapitalValidator() ]),
     picture: new FormControl('assets/default-user.png'),
-    phone: new FormGroup({
-      type: new FormControl(null),
-      number: new FormControl('')
-    }),
+    phones: new FormArray([
+      new FormGroup({
+        type: new FormControl(null),
+        number: new FormControl('')
+      })
+    ]),
     email: new FormControl(''),
     address: new FormControl('')
   });
 
-  constructor(private contactsService:ContactsService) { }
+  constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
     const contact = localStorage.getItem('contact');
-    if(contact){
-      this.contactForm.setValue(JSON.parse(contact));
+    if (contact) {
+      const contactJSON = JSON.parse(contact);
+      this.phones.clear();
+      for (let i = 0; i < contactJSON.phones.length; i++) {
+          this.addNewPhoneToModel();
+      }
+      this.contactForm.setValue(contactJSON);
     }
-    
+
     zip(this.contactForm.statusChanges, this.contactForm.valueChanges).pipe(
       filter( ([state, value]) => state == 'VALID' ),
       map(([state, value]) => value),
@@ -42,25 +49,35 @@ export class ContactFormComponent implements OnInit {
     });
   }
 
-  addContact(){
+  addContact() {
   }
 
-  addNewPhoneToModel(){
+  addNewPhoneToModel() {
+    this.phones.push(
+      new FormGroup({
+        type: new FormControl(null),
+        number: new FormControl(''),
+      })
+    );
   }
 
-  addImage(event){
+  addImage(event) {
     const file = event.target.files[0];
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = (evt) => {
       this.contactForm.patchValue({
-        picture:reader.result
+        picture: reader.result
       });
-    }
+    };
   }
 
-  get name(){
+  get name() {
     return this.contactForm.get('name');
+  }
+  
+  get phones(){
+    return this.contactForm.get('phones') as FormArray;
   }
 
 }
